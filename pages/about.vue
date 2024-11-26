d4b086e187fbcf68616e2dacf6ea3566047d2c66
rasung.woo <rasung.woo@sk.com>
2024-11-26 11:04:43 +0900

    RouterLink와 NuxtLink의 차이

--- pages/about.vue
+++ pages/about.vue
@@ -5,6 +5,13 @@
       <p class="q-mt-sm text-grey-8">
         Vue & Nuxt Mastery Class 웹은 Nuxt3로 만들어졌습니다.
       </p>
+      <div class="column">
+        <RouterLink to="/">RouterLink Home</RouterLink>
+        <RouterLink to="https://youtube.com">RouterLink Youtube</RouterLink>
+        <br />
+        <NuxtLink to="/">RouterLink Home</NuxtLink>
+        <NuxtLink to="https://youtube.com">RouterLink Youtube</NuxtLink>
+      </div>
     </div>
   </q-page>
 </template>
